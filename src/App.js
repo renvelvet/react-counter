c96e5469b2508911e746ac28ddc9e58ebb5abe5f
Resha Puspita <reshapuspita734@gmail.com>
2020-07-02 10:48:59 +0700

    count class

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import "./App.css";
 import CountFunction from "./components/CountFunction/CountFunction";
 import CountClass from "./components/CountClass/CountClass";
 
+import "./App.css";
 function App() {
   return (
     <div className="app">
---
+++ src/components/CountClass/CountClass.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from "react";
+import "./CountClass.css";
+export default class CountClass extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      num: 0,
+    };
+  }
+  addNumber = () => {
+    this.setState((state) => ({
+      num: state.num + 1,
+    }));
+  };
+  minNumber = () => {
+    if (this.state.num <= 0) {
+      alert("Angka tidak boleh negatif!");
+    } else {
+      this.setState((state) => ({
+        num: state.num - 1,
+      }));
+    }
+  };
+  resetNumber = () => {
+    this.setState((state) => ({ num: 0 }));
+  };
+
+  render() {
+    return (
+      <div className="countC">
+        <h1>
+          Count: <span>{this.state.num}</span>
+        </h1>
+        <button onClick={this.addNumber}>+</button>
+        <button onClick={this.minNumber}>-</button>
+        <button onClick={this.resetNumber}>RESET</button>
+        {/* <button onClick={() => setnumber(number - 1)}>-</button>
+        <button onClick={() => setnumber(0)}>RESET</button> */}
+      </div>
+    );
+  }
+}
