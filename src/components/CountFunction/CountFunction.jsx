37f81273f48820d1c06ad09891df35ba87c3fb2b
Resha Puspita <reshapuspita734@gmail.com>
2020-07-02 10:58:36 +0700

    update count function yang masih bug

--- src/components/CountFunction/CountFunction.jsx
+++ src/components/CountFunction/CountFunction.jsx
@@ -3,16 +3,23 @@ import "./CountFunction.css";
 function CountFunction() {
   const [number, setnumber] = useState(0);
 
-  if (number < 0) {
-    alert("Angka tidak boleh negatif!");
-  }
   return (
     <div className="countF">
       <h1>
         Count: <span>{number}</span>
       </h1>
       <button onClick={() => setnumber(number + 1)}>+</button>
-      <button onClick={() => setnumber(number - 1)}>-</button>
+      <button
+        onClick={() => {
+          if (number <= 0) {
+            alert("Angka tidak boleh negatif!");
+          } else {
+            setnumber(number - 1);
+          }
+        }}
+      >
+        -
+      </button>
       <button onClick={() => setnumber(0)}>RESET</button>
     </div>
   );
